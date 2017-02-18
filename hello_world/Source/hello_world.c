de22c5c67e8eab08a0d73c2b497cb79aba0bbbd0
guany <guany@guany-B85M-D3V>
2017-02-18 22:20:08 +0800

    1st commit

---
+++ hello_world/Source/hello_world.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : hello_world.c
+ Author      : guany
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("master"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
